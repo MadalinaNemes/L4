6078a0ce50567c3e670be6cd251cc7ab340f20ce
Madalina <madalina_cristina_14@yahoo.com>
2017-05-10 10:24:34 +0300

    Initial commit

---
+++ src/test/java/repository/FileDataPersistenceTest.java
@@ -0,0 +1,46 @@
+package repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Madalina on 5/10/2017.
+ */
+public class FileDataPersistenceTest {
+    @Test
+    public void getFile() throws Exception {
+
+    }
+
+    @Test
+    public void setFile() throws Exception {
+
+    }
+
+    @Test
+    public void saveStudent() throws Exception {
+
+    }
+
+    @Test
+    public void saveLaboratory() throws Exception {
+
+    }
+
+    @Test
+    public void addGrade() throws Exception {
+
+    }
+
+    @Test
+    public void getLaboratoryMap() throws Exception {
+
+    }
+
+    @Test
+    public void getStudentsList() throws Exception {
+
+    }
+
+}
